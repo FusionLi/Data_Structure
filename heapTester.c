125e6d606550c6f9698476852b0b878a6b72a60d
ZeqiLi <Zeqi Li>
2016-01-30 20:09:33 -0500

    with basic functions of heap: insert, print(preorder) and destroy without memory leak

--- heapTester.c
+++ heapTester.c
@@ -20,9 +20,16 @@ int main() {
 		else if (strcmp(cmd, "insert") == 0 && cmdOrderCheck == 1) {
 			printf("Data: ");
 			scanf("%d", &data);
-			Insert(heap, data);
+			int insertType = Insert(heap, data);
+			if (insertType == 0) 
+				printf("\nInsert without modification\n");
+			else if (insertType == 1) 
+				printf("\nInsert with modification\n");
+			else
+				printf("\nInsertion failure\n");
 		}
 		else if (strcmp(cmd, "print") == 0 && cmdOrderCheck == 1) {
+			printf("heap size: %d\n", heap->count);
 			PrintHeap(heap->heapHead);
 		}
 		else if (strcmp(cmd, "destroy") == 0 && cmdOrderCheck == 1) {
